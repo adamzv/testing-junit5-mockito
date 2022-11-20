06bfb9371a020ae4a1c282e942d09e853e0f60a9
Adam Zverka <adamzverka@hotmail.com>
2022-11-20 21:00:28 +0100

    Intro to mockito

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,38 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService service;
+
+    @Test
+    void deleteById() {
+        service.deleteById(1l);
+        service.deleteById(1l);
+
+        verify(specialtyRepository, times(2)).deleteById(1l);
+        // times() atLeastOnce() atMost
+
+        verify(specialtyRepository, never()).deleteById(5l);
+    }
+
+    @Test
+    void testDelete() {
+        service.delete(new Speciality());
+    }
+}
