c2b5424db63f13eed65192e7e6193aa94dec07ad
Adam Zverka <adamzverka@hotmail.com>
2022-11-26 20:40:53 +0100

    Started mocking Jpa service

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -19,6 +22,28 @@ class SpecialitySDJpaServiceTest {
 
     @InjectMocks
     SpecialitySDJpaService service;
+
+    @Test
+    void delete() {
+        Speciality speciality = new Speciality();
+
+        service.delete(speciality);
+
+        verify(specialtyRepository).delete(any(Speciality.class));
+    }
+
+    @Test
+    void findById() {
+        Speciality speciality = new Speciality();
+
+        when(specialtyRepository.findById(1l)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1l);
+
+        assertThat(foundSpeciality).isNotNull();
+
+        verify(specialtyRepository).findById(anyLong());
+    }
 
     @Test
     void deleteById() {
